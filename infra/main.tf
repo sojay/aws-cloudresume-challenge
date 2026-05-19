de28c9b6f5b4a280f25fd1e4bd8298a34ffe8b54
Samuel <sojaysings@gmail.com>
2026-05-19 17:16:26 -0400

    chore: cleanup stale Lambda + Fucntion URL
    applied visitor logging fix from browser localStorage
    
    converted resume site to Svelte 5

--- infra/main.tf
+++ infra/main.tf
@@ -1,85 +1,149 @@
-resource "aws_lambda_function" "myfunc" {
-  filename         = data.archive_file.zip_the_python_code.output_path
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "myfunc"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.lambda_handler"
-  runtime          = "python3.9"
+locals {
+  aws_region = "ca-central-1"
+  account_id = "783038225397"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+resource "aws_dynamodb_table" "cloudresume_test" {
+  name           = "cloudresume-test"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 5
+  hash_key       = "id"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    project = "Cloud Resume Challenge"
+  }
+
+  ttl {
+    attribute_name = "expires_at"
+    enabled        = true
+  }
 }
-EOF
-}
 
-resource "aws_iam_policy" "iam_policy_for_resume_project" {
+resource "aws_iam_role" "cloud_resume_api_role" {
+  name = "cloud-resume-test-api-role-h8eue2z6"
+  path = "/service-role/"
 
-  name        = "aws_iam_policy_for_terraform_resume_project_policy"
-  path        = "/"
-  description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ],
-          "Resource" : "arn:aws:logs:*:*:*",
-          "Effect" : "Allow"
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "dynamodb:UpdateItem",
-			"dynamodb:GetItem",
-            "dynamodb:PutItem"
-          ],
-          "Resource" : "arn:aws:dynamodb:*:*:table/cloudresume-test"
-        },
-      ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
-  
+resource "aws_iam_policy" "lambda_basic_execution" {
+  name = "AWSLambdaBasicExecutionRole-5b17fb43-2d84-41a3-a227-e4b2cbfe47eb"
+  path = "/service-role/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "logs:CreateLogGroup"
+        Resource = "arn:aws:logs:${local.aws_region}:${local.account_id}:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Resource = [
+          "arn:aws:logs:${local.aws_region}:${local.account_id}:log-group:/aws/lambda/cloud-resume-test-api:*",
+        ]
+      },
+    ]
+  })
 }
 
-data "archive_file" "zip_the_python_code" {
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.cloud_resume_api_role.name
+  policy_arn = aws_iam_policy.lambda_basic_execution.arn
+}
+
+resource "aws_iam_policy" "visitor_table_access" {
+  name = "cloud-resume-test-api-visitor-table-access"
+  path = "/service-role/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+        ]
+        Resource = aws_dynamodb_table.cloudresume_test.arn
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "visitor_table_access" {
+  role       = aws_iam_role.cloud_resume_api_role.name
+  policy_arn = aws_iam_policy.visitor_table_access.arn
+}
+
+
+resource "aws_cloudwatch_log_group" "cloud_resume_api" {
+  name              = "/aws/lambda/cloud-resume-test-api"
+  retention_in_days = 30
+}
+
+data "archive_file" "cloud_resume_api_zip" {
   type        = "zip"
-  source_file = "${path.module}/lambda/func.py"
-  output_path = "${path.module}/lambda/func.zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda/lambda_function.zip"
 }
 
-resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.myfunc.function_name
+resource "aws_lambda_function" "cloud_resume_api" {
+  filename         = data.archive_file.cloud_resume_api_zip.output_path
+  source_code_hash = data.archive_file.cloud_resume_api_zip.output_base64sha256
+  function_name    = "cloud-resume-test-api"
+  role             = aws_iam_role.cloud_resume_api_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+
+  architectures = ["x86_64"]
+  memory_size   = 128
+  timeout       = 3
+
+  environment {
+    variables = {
+      VISITOR_EVENT_TTL_DAYS = "90"
+      VISITOR_TABLE_NAME     = aws_dynamodb_table.cloudresume_test.name
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic_execution,
+    aws_iam_role_policy_attachment.visitor_table_access,
+    aws_cloudwatch_log_group.cloud_resume_api,
+  ]
+}
+
+resource "aws_lambda_function_url" "cloud_resume_api" {
+  function_name      = aws_lambda_function.cloud_resume_api.function_name
   authorization_type = "NONE"
+  invoke_mode        = "BUFFERED"
 
   cors {
-    allow_credentials = true
-    allow_origins     = ["*"]
-    allow_methods     = ["*"]
-    allow_headers     = ["date", "keep-alive"]
-    expose_headers    = ["keep-alive", "date"]
-    max_age           = 86400
+    allow_origins = ["*"]
   }
 }
